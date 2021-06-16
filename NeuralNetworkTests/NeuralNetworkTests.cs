9f1c596920a723a39a86a2933d8de48d4a993fdd
Danil <danil.maskin@inbox.ru>
2021-06-16 07:48:32 +0300

    Added scaling and normalization methods.

--- NeuralNetworkTests/NeuralNetworkTests.cs
+++ NeuralNetworkTests/NeuralNetworkTests.cs
@@ -10,7 +10,8 @@ namespace NeuralNetwork.Tests
         [TestMethod()]
         public void FeedForwardTest()
         {
-            var dataset = new List<Tuple<double, double[]>>
+            var outputs = new double[] { 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1 };
+            var inputs = new double[,]
             {
                 //  Result
                 //  The patient is sick - 1
@@ -20,39 +21,40 @@ namespace NeuralNetwork.Tests
                 // Good age - A
                 // Smokes - S
                 // Eats right - K 
-                //                                                                     T  A  S  K
-                new Tuple<double, double[]> (0, new double[] { 0, 0, 0, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 0, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 0, 0, 1, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 0, 1, 1 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 1, 0, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 1, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 0, 1, 1, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 1, 1, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 0, 0 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 1, 0 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 1, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 1, 0, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 1, 1, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 1, 1, 0 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 1, 1, 1 })
+                //T  A  S  K
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 1 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 1, 1 },
+                { 0, 1, 0, 0 },
+                { 0, 1, 0, 1 },
+                { 0, 1, 1, 0 },
+                { 0, 1, 1, 1 },
+                { 1, 0, 0, 0 },
+                { 1, 0, 0, 1 },
+                { 1, 0, 1, 0 },
+                { 1, 0, 1, 1 },
+                { 1, 1, 0, 0 },
+                { 1, 1, 0, 1 },
+                { 1, 1, 1, 0 },
+                { 1, 1, 1, 1 }
             };
 
-            var topology = new Topology(4, 1, 0.01, 4);
+            var topology = new Topology(4, 1, 0.1, 2);
             var neuralNetwork = new NeuralNetwork(topology);
-            var difference = neuralNetwork.Learn(dataset, 800000);
+            var difference = neuralNetwork.Learn(outputs, inputs, 80000);
 
             var results = new List<double>();
-            foreach (var data in dataset)
+            for (int i = 0; i < outputs.Length; i++)
             {
-                var res = neuralNetwork.FeedForward(data.Item2).Output;
+                var row = NeuralNetwork.GetRow(inputs, i);
+                var res = neuralNetwork.FeedForward(row).Output;
                 results.Add(res);
             }
 
             for (int i = 0; i < results.Count; i++)
             {
-                var expected = Math.Round(dataset[i].Item1, 3);
+                var expected = Math.Round(outputs[i], 3);
                 var actual = Math.Round(results[i], 3);
                 Assert.AreEqual(expected, actual);
             }
